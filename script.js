75e9eba307774a0989c8bab3a57eb81a38e01d32
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-30 13:48:09 -0700

    Add files via upload
    Master Browser Control: Detect and Handle Popup Blockers in JavaScript!

---
+++ script.js
@@ -0,0 +1,27 @@
+// Browser control: Testing for popup blockers
+
+
+
+// TEST FOR POPUP BLOCKER 
+
+// var testPop = window.open("","","height=100,width=100");
+// if (testPop === null) {
+//     alert("please disable your popup blocker");
+// } else{
+//     alert("popup blocker is disabled");
+//     testPop.close();
+// }
+
+
+
+// USING ABOVE CODE AS A FUNCTION
+
+// function checkforPopBlocker(){
+//     var testPop = window.open("","","height=100,width=100");
+// if (testPop === null) {
+//     alert("please disable your popup blocker");
+// } else{
+//     alert("popup blocker is disabled");
+//     testPop.close();
+// }
+// }
